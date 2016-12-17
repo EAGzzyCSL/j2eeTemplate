5529ae980e23355ded1f182b788fee2a0ad738ec
EAGzzyCSL <eagzzycsl@outlook.com>
2016-12-17 21:33:31 +0800

    提交代码

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # j2eeTemplate
 使用idea做j2ee开发的一个简单模板，抛弃myeclipse吧。
+
+关于项目的配置，可以参见[这里](https://eagzzycsl.github.io/app/Blog/index.html#使用ideal社区版搭建jsp开发环境)
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * 主要用途：
+ * 以java程序的方式运行普通的java类，免去在网页上测试jsp和servlet
+ */
+public class Main {
+    public static void main(String[] args) throws IllegalAccessException {
+        System.out.println("hello world");
+    }
+}
+
